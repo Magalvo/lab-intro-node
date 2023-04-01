331c4dd9da033a467cb90e1cd8a1bd8a03949508
Diogo Santos <diogo.magalhaescalvo@gmail.com>
2023-04-01 14:45:19 +0100

    Done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList');
+    } else {
+      const arr = this.items;
+      const max = arr.reduce((a, b) => Math.max(a, b), -Infinity);
+      return max;
+    }
+  }
 
-  min() {}
+  min() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (!this.items.length) {
+      return 0;
+    } else {
+      const sum = this.items.reduce((a, b) => a + b, 0);
+      return sum;
+    }
+  }
 
-  avg() {}
+  avg() {
+    if (!this.items.length) {
+      throw new Error('EmptySortedList');
+    } else {
+      const avg = this.items.reduce((a, b) => a + b, 0);
+      return avg / this.length;
+    }
+  }
 }
 
 module.exports = SortedList;
